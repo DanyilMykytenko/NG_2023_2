8ab375d406eabf71200d186d6b7e8dde7e8bd9f7
DaniylMykytenko <freezeee682@gmail.com>
2023-07-24 20:48:10 +0300

    I am sorry for this

---
+++ ChallengeMe/ChallengeMe/CONFIDENT/NotUsersAtAll.cs
@@ -0,0 +1,39 @@
+﻿namespace ChallengeMe.CONFIDENT
+{
+    internal static class NotUsersAtAll
+    {
+        static NotUsersAtAll()
+        {
+            var ah = new NotUser(); 
+            ah.SetNotValues("1", "2");
+            hohoho[0] = ah;
+
+
+
+
+
+            return;
+        }
+        public static NotUser[] hohoho = new NotUser[1];
+    }
+
+    internal class NotUser
+    {
+        public void SetNotValues(string String, string _String)
+        {
+            notPess123word = String;
+            NotName = _String;
+        }
+
+        string NotName;string notPess123word;
+
+        public string GetNotName()
+        {
+            return notPess123word;
+        }
+        public string GetNotNumbers()
+        {
+            return NotName;
+        }
+    }
+}
