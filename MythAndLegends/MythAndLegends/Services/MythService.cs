7834ca7b2260bf9cc06915604205295c796acce2
Andrii Nebesnyi <artpro197@gmail.com>
2023-07-25 19:48:14 +0300

    Task 2 for Epic 2

---
+++ MythAndLegends/MythAndLegends/Services/MythService.cs
@@ -0,0 +1,39 @@
+﻿using MythAndLegends.Data;
+using MythAndLegends.Data.Entity;
+using MythAndLegends.Services.Interface;
+
+namespace MythAndLegends.Services;
+
+public class MythService : IStoryService
+{
+    public void AddMyth(Myth myth)
+    {
+        if (string.IsNullOrEmpty(myth.StoryCode))
+        {
+            myth.StoryCode = CreateCode(myth.Name);
+        }
+
+        Storage.Myths.Add(myth);
+    }
+
+    public Story? GetMythByCode(string code)
+    {
+        return Storage.Myths.FirstOrDefault(x => x.StoryCode.Equals(code));
+    }
+
+    public void AddLegend(Legend legend)
+    {
+    }
+    
+    public Story? GetLegendByCode(string code)
+    {
+        return null;
+    }
+    
+    private string CreateCode(string name)
+    {
+        var code = $"{name.First()}{name.Last()}-{name.Length}";
+
+        return code;
+    }
+}
